4984f0eed2593d3521348d970347ca37b689c2f1
Rael Bernardino <rbernardino@pos360.com>
2022-08-22 19:58:02 -0700

    other: Add Contenful boilerplate

---
+++ getContentfulEnvironment.ts
@@ -0,0 +1,14 @@
+import contentfulManagement from "contentful-management";
+require("dotenv").config();
+
+module.exports = function () {
+  const contentfulClient = contentfulManagement.createClient({
+    accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN ?? "",
+  });
+
+  return contentfulClient
+    .getSpace(process.env.CONTENTFUL_SPACE_ID ?? "")
+    .then((space) =>
+      space.getEnvironment(process.env.CONTENTFUL_ENVIRONMENT ?? "")
+    );
+};
